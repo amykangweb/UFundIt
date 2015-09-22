a343f1bd19e8a4b44558838e345effd0fa0c7c1b
amykangweb <selene6023@gmail.com>
2015-09-21 17:19:02 -0700

    Create method in deals model to return deals created by current user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :commitments
-  has_many :deals
-  has_many :commited_deals, through: :commitments, class_name: "Deal"
+  has_many :deals, through: :commitments
+  has_many :committed_deals, through: :commitments, class_name: "Deal"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
