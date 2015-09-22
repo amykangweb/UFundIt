8207584b012b751bed6f0a72a4b1dc03a086bc74
amykangweb <selene6023@gmail.com>
2015-09-22 16:28:37 -0700

    Test to check if only published and public search results are returned.

--- test/features/deals/search_deals_test.rb
+++ test/features/deals/search_deals_test.rb
@@ -8,4 +8,11 @@ feature "Search Form" do
     page.text.must_include "Widgets"
     page.text.wont_include "Seahawks Suite for Panthers Game"
   end
+
+  scenario "only returns published and non-private results" do
+    visit root_path
+    fill_in "search", with: "Seahawks"
+    click_on "Search"
+    page.text.wont_include "Seahawks"
+  end
 end
