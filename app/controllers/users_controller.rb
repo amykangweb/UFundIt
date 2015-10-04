e89eb535639f114cf729f901b8c0095fd993680a
amykangweb <selene6023@gmail.com>
2015-10-03 22:02:56 -0700

    Add will paginate gem and paginate deals on user profile page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @deals = @user.owned_deals.paginate(page: params[:page], per_page: 10)
+    @committed = @user.deals.paginate(page: params[:page], per_page: 10)
   end
 end
